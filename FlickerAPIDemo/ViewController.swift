bbbcf010b2b97bd62ece8a38f7de55590775b4b2
will <will.weng@lig.com.tw>
2022-01-26 11:21:27 +0800

    Initial Commit

---
+++ FlickerAPIDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlickerAPIDemo
+//
+//  Created by will on 2022/1/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
